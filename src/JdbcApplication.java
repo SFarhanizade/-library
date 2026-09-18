2190399397b3e9907ded0dc85d7b3bee50ec5d7c
atefeheydar <atefe.heydar@gmail.com>
2026-09-18 15:51:55 +0330

    edit add jdbc connection

--- src/JdbcApplication.java
+++ src/JdbcApplication.java
@@ -11,38 +11,38 @@ void main() throws SQLException {
 }
 
 
-    private static void createTable (Connection connection) throws SQLException {
+private static void createTable(Connection connection) throws SQLException {
 
-        String query = "CREATE TABLE IF NOT EXISTS TB_BOOK (" + "id SERIAL PRIMARY KEY ," +
-                "title varchar(30) NOT NULL, " +
-                "author varchar(30) NOT NULL ,"+
-                "available boolean" +
-                ");" ;
+    String query = "CREATE TABLE IF NOT EXISTS TB_BOOK (" + "id SERIAL PRIMARY KEY ," +
+            "title varchar(30) NOT NULL, " +
+            "author varchar(30) NOT NULL ," +
+            "available boolean" +
+            ");";
 
-        try (Statement statement = connection.createStatement()) {
-                statement.execute(query);
-            }
+    try (Statement statement = connection.createStatement()) {
+        statement.execute(query);
+    }
 
-        query = "CREATE TABLE IF NOT EXISTS TB_MEMBER (" + "id SERIAL PRIMARY KEY ," +
-                "name varchar(30) NOT NULL, " +
-                "tel varchar(20) NOT NULL ,"+
-                "address varchar(30)," +
-                "email varchar(50)" +
-                ");" ;
+    query = "CREATE TABLE IF NOT EXISTS TB_MEMBER (" + "id SERIAL PRIMARY KEY ," +
+            "name varchar(30) NOT NULL, " +
+            "tel varchar(20) NOT NULL ," +
+            "address varchar(30)," +
+            "email varchar(50)" +
+            ");";
 
-        try (Statement statement = connection.createStatement()) {
-                statement.execute(query);
-            }
+    try (Statement statement = connection.createStatement()) {
+        statement.execute(query);
+    }
 
-        query = "CREATE TABLE IF NOT EXISTS loan (" + "id SERIAL PRIMARY KEY ," +
-                "loan_date date NOT NULL, " +
-                "return_date date NOT NULL ,"+
-                "book_id int REFERENCES book," +
-                "member_id int REFERENCES member" +
-                ");" ;
+    query = "CREATE TABLE IF NOT EXISTS loan (" + "id SERIAL PRIMARY KEY ," +
+            "loan_date date NOT NULL, " +
+            "return_date date NOT NULL ," +
+            "book_id int REFERENCES book," +
+            "member_id int REFERENCES member" +
+            ");";
 
-        try (Statement statement = connection.createStatement()) {
-                statement.execute(query);
-            }
-        }
+    try (Statement statement = connection.createStatement()) {
+        statement.execute(query);
+    }
+}
 
