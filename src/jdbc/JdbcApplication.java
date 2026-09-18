3487a5ef1ed4dc7c22abf0981a22cb561f91bf9b
atefeheydar <atefe.heydar@gmail.com>
2026-09-18 16:30:32 +0330

    add tables

--- src/jdbc/JdbcApplication.java
+++ src/jdbc/JdbcApplication.java
@@ -13,7 +13,7 @@ void main() throws SQLException {
 
 private static void createTable(Connection connection) throws SQLException {
 
-    String query = "CREATE TABLE IF NOT EXISTS TB_BOOK (" + "id SERIAL PRIMARY KEY ," +
+    String query = "CREATE TABLE IF NOT EXISTS TB_BOOK (" + "id int PRIMARY KEY ," +
             "title varchar(30) NOT NULL, " +
             "author varchar(30) NOT NULL ," +
             "available boolean" +
@@ -23,7 +23,7 @@ private static void createTable(Connection connection) throws SQLException {
         statement.execute(query);
     }
 
-    query = "CREATE TABLE IF NOT EXISTS TB_MEMBER (" + "id SERIAL PRIMARY KEY ," +
+    query = "CREATE TABLE IF NOT EXISTS TB_MEMBER (" + "id int PRIMARY KEY ," +
             "name varchar(30) NOT NULL, " +
             "tel varchar(20) NOT NULL ," +
             "address varchar(30)," +
@@ -34,12 +34,16 @@ private static void createTable(Connection connection) throws SQLException {
         statement.execute(query);
     }
 
-    query = "CREATE TABLE IF NOT EXISTS loan (" + "id SERIAL PRIMARY KEY ," +
-            "loan_date date NOT NULL, " +
-            "return_date date NOT NULL ," +
-            "book_id int REFERENCES book," +
-            "member_id int REFERENCES member" +
-            ");";
+
+    query = "        CREATE TABLE IF NOT EXISTS loan " +
+            "(id SERIAL PRIMARY KEY,           " +
+            " loan_date DATE NOT NULL,            " +
+            "return_date DATE NOT NULL,            " +
+            "member_id INT NOT NULL,            " +
+            "book_id INT NOT NULL,           " +
+            " CONSTRAINT fk_loan_member FOREIGN KEY (member_id) REFERENCES TB_MEMBER(id), " +
+            " CONSTRAINT fk_loan_book FOREIGN KEY (book_id) REFERENCES TB_BOOK(id)      " +
+            "  );        ";
 
     try (Statement statement = connection.createStatement()) {
         statement.execute(query);
