589facef03b69dcd7d56185d888dbcde2a0cf36a
atefeheydar <atefe.heydar@gmail.com>
2026-09-20 10:11:28 +0330

    id type change

--- library.sql
+++ library.sql
@@ -1,6 +1,6 @@
 CREATE TABLE member
 (
-    id      int PRIMARY KEY,
+    id      SERIAL PRIMARY KEY,
     name    varchar(30) NOT NULL,
     tel     varchar(20) NOT NULL,
     address varchar(50),
@@ -8,7 +8,7 @@ CREATE TABLE member
 );
 CREATE TABLE book
 (
-    id        int PRIMARY KEY,
+    id        SERIAL PRIMARY KEY,
     title     varchar(25) NOT NULL,
     author    varchar(30) NOT NULL,
     available boolean DEFAULT TRUE
@@ -16,7 +16,7 @@ CREATE TABLE book
 
 CREATE TABLE loan
 (
-    id        int PRIMARY KEY,
+    id        SERIAL PRIMARY KEY,
     book_id   int  NOT NULL,
     member_id int  NOT NULL,
     loan_date date NOT NULL,
