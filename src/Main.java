33a8809e1cf1d3c18002fa0c8eacdb43d6988ab2
atefeheydar <atefe.heydar@gmail.com>
2026-09-20 10:16:01 +0330

    test main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
-void main() {
+import repository.MemberRepository;
 
+import java.sql.SQLException;
 
+static void main(String[] args) throws SQLException {
 
+    MemberRepository memberRepository = new MemberRepository();
 
+    IO.println(memberRepository.findById(1));
     }
