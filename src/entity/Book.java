d2b72ac439e25a2b4f162a06edb5a5ed37cfcf42
gildahosseini <gildahosseini1998@gmail.com>
2026-09-23 20:07:41 +0330

    add book to entity

---
+++ src/entity/Book.java
@@ -0,0 +1,57 @@
+package entity;
+
+public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private boolean available;
+
+    public Book(int id, String title, String author, boolean available) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.available = available;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", available=" + available +
+                '}';
+    }
+}
