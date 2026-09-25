fc7027f6bfc8816241c355faef8d8bdad4035f84
atefeheydar <atefe.heydar@gmail.com>
2026-09-25 14:38:52 +0330

    fix: rename add to save

--- src/service/BookService.java
+++ src/service/BookService.java
@@ -29,7 +29,7 @@ public class BookService {
         newbook.setTitle(title);
         newbook.setAvailable(available);
 
-        bookRepository.add(newbook);
+        bookRepository.save(newbook);
 
     }
 }
